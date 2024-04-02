986be95676ce5a5ad54de06e94df1cb81c871e99
abhisheksharma402 <abhib12402@gmail.com>
2024-04-02 12:14:11 +0530

    added new origin

--- src/main/java/com/example/webrtc/demo/security/Conf.java
+++ src/main/java/com/example/webrtc/demo/security/Conf.java
@@ -10,7 +10,7 @@ public class Conf implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // Allow all origins (you can specify specific origins here)
+                .allowedOrigins("https://660ba7c68189eb0008ee425d--dainty-dusk-b4dc92.netlify.app/") // Allow all origins (you can specify specific origins here)
                 .allowedMethods("*"); // Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
     }
 }
